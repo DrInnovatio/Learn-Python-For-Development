d522d936925afac53e83d58544aec44ae9079a83
DrInnovatio <turing1723@gmail.com>
2020-05-10 18:51:41 +1000

    Euclid algorithm

--- Algorithm/greatestCommonDivisor.py
+++ Algorithm/greatestCommonDivisor.py
@@ -1,5 +1,5 @@
 def gcd(a, b):
-    i = min(a, b)  # To choose and store minimum value
+    i = min(a, b)   # To choose and store minimum value
     while True:
         if a % i == 0 and b % i == 0:
             return i
@@ -10,3 +10,15 @@ print(gcd(1, 5))
 print(gcd(3, 6))
 print(gcd(60, 24))
 print(gcd(81, 27))
+
+
+def Euclid(c, d):
+    if d == 0:
+        return c
+    return Euclid(d, c % d)
+
+
+print(Euclid(1, 5))
+print(Euclid(3, 6))
+print(Euclid(60, 24))
+print(Euclid(81, 27))
