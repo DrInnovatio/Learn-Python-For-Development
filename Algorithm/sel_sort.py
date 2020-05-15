c580e240279ea2a10aadca2de8f1b5be844eedbc
DrInnovatio <turing1723@gmail.com>
2020-05-15 22:37:20 +1000

    Insertion Sort

--- Algorithm/sel_sort.py
+++ Algorithm/sel_sort.py
@@ -6,6 +6,7 @@ def sel_sort(a):
         for j in range(i + 1, n):
             if a[j] < a[min_idx]:
                 min_idx = j
+
         a[i], a[min_idx] = a[min_idx], a[i]
 
 
@@ -13,3 +14,12 @@ d = [3, 5, 1, 2, 6, 4, 7]
 
 sel_sort(d)
 print(d)
+
+
+f = 1
+g = 2
+h = 3
+
+f,g,h = h,f,g
+
+print(f,g,h)
