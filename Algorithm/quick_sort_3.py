0cda6f50862907439236c33695e4bbd52eac6b7a
DrInnovatio <turing1723@gmail.com>
2020-05-22 18:30:57 +1000

    Binary Search

--- Algorithm/quick_sort_3.py
+++ Algorithm/quick_sort_3.py
@@ -14,9 +14,11 @@ def quick_sort_sub(a, start, end):
     quick_sort_sub(a, start, i - 1)
     quick_sort_sub(a, i + 1, end)
 
+
 def quick_sort(a):
     quick_sort_sub(a, 0, len(a) - 1)
 
-d = [6,8,3,9,10,1,2,4,7,5]
+
+d = [6, 8, 3, 9, 10, 1, 2, 4, 7, 5]
 quick_sort(d)
 print(d)
